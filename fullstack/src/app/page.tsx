2686afe51ac15f11720af6206858786eacb5d0e1
Josh <116848739+joshuaeduque@users.noreply.github.com>
2025-04-04 11:49:27 -0500

    Homepage tweaks (#37)

--- fullstack/src/app/page.tsx
+++ fullstack/src/app/page.tsx
@@ -38,41 +38,41 @@ export default function Home() {
     authenticated,
     cartOpen,
     setSearchText,
-    setCartOpen,} = useHeader();
-  
-    useEffect(() => {
-      getProducts()
-        .then((products) => setFeaturedProducts(products))
-        .catch((error) => {
-          setError(true);
-          console.error(error);
-        })
-        .finally(() => setLoading(false));
-    }, []);
-  
-    // Fetch all products for CartSheet and product grid
-    useEffect(() => {
-      getAllProductDocuments()
-        .then((docs) => {
-          // Map QueryDocumentSnapshot to Product[]
-          const allProductsData = docs.map((doc) => {
-            const data = doc.data();
-            return {
-              id: doc.id,
-              name: data.name,
-              price: data.price,
-              imageSrc: data.imageSrc,
-              collectionName: data.collectionName, // If needed for routing
-            } as Product;
-          });
-          setProducts(allProductsData);
-        })
-        .catch((error) => {
-          setError(true);
-          console.error(error);
+    setCartOpen, } = useHeader();
+
+  useEffect(() => {
+    getProducts()
+      .then((products) => setFeaturedProducts(products))
+      .catch((error) => {
+        setError(true);
+        console.error(error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  // Fetch all products for CartSheet and product grid
+  useEffect(() => {
+    getAllProductDocuments()
+      .then((docs) => {
+        // Map QueryDocumentSnapshot to Product[]
+        const allProductsData = docs.map((doc) => {
+          const data = doc.data();
+          return {
+            id: doc.id,
+            name: data.name,
+            price: data.price,
+            imageSrc: data.imageSrc,
+            collectionName: data.collectionName, // If needed for routing
+          } as Product;
         });
-    }, []);
-  
+        setProducts(allProductsData);
+      })
+      .catch((error) => {
+        setError(true);
+        console.error(error);
+      });
+  }, []);
+
 
   const images = [
     '/depositphotos_227387246-stock-photo-photo-of-three-witches-with.jpg',
@@ -83,31 +83,17 @@ export default function Home() {
     "Beginner",
     "More Categories"
   ];
-  
+
   return (
     <div>
-      <SiteHeader authenticated={authenticated} setSearchText={setSearchText} onSearchClicked={handleSearchClicked} onAuthClicked={handleAuthClicked} onCartClicked={handleCartClicked} onAccountClicked={handleAccountClicked}/>
-      
-      {/* Code From Chris */}
+      <SiteHeader authenticated={authenticated} setSearchText={setSearchText} onSearchClicked={handleSearchClicked} onAuthClicked={handleAuthClicked} onCartClicked={handleCartClicked} onAccountClicked={handleAccountClicked} />
 
-      <div className='flex justify-center'>
-        <Carousel className='w-full max-w-xl' id="hero-carousel" opts={{
-          loop: true
-        }}>
+      <div className='flex justify-center py-4'>
+        <Carousel className='w-full max-w-xl' id="hero-carousel" opts={{ loop: true }}>
           <CarouselContent>
             {images.map((value, index) => (
-              <CarouselItem key={index}>
-                <div className="p-1">
-                  <Card>
-                    <CardContent className="flex items-center justify-center">
-                      <Image
-                          src={value}
-                          alt="Image"
-                          height={500}
-                          width={500}/>
-                    </CardContent>
-                  </Card>
-                </div>
+              <CarouselItem className="flex content-center" key={index}>
+                <img src={value} alt={"fuck me man"} />
               </CarouselItem>
             ))}
           </CarouselContent>
@@ -116,11 +102,8 @@ export default function Home() {
         </Carousel>
       </div>
 
-      <div className="px-4 py-2">
-        <h1>Collections</h1>
-      </div>
-
-      <div className='flex justify-center w-full px-[10%]'>
+      <div className='flex flex-col gap-4 justify-center w-full px-[10%] py-4'>
+        <p>Collections</p>
         <Carousel className='w-full'>
           <CarouselContent>
             {categories.map((value, index) => (
@@ -136,86 +119,43 @@ export default function Home() {
           <CarouselNext />
           <CarouselPrevious />
         </Carousel>
-      </div>
-      <div className="px-4 py-2">
-        <h1>Featured</h1>
-      </div>
 
-      <div className='flex justify-center w-full px-[10%]'>
-        <Carousel className='w-full'>
-          <CarouselContent>
-            {
-              featuredProducts?.map((product) => {
-                return (
-                  <CarouselItem className='basis-1/5' key={product.id}
-                  onClick={() =>
-                    router.push(`/productPage/${product.collectionName}/${product.id}`)
-                  }
-                  style={{ cursor: "pointer" }}>
-                    <ProductCard
-                      key={product.id}
-                      name={product.name}
-                      price={product.price}
-                      imageSrc={product.imageSrc}
-                    />
-                  </CarouselItem>
-                );
-              })}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
-      </div>
-
-      {/* Code From DEV */}
-
-      <div className="px-4 py-1 border-b border-gray-800 flex justify-end">
-        <DropdownMenu>
-          <DropdownMenuTrigger>Sort by</DropdownMenuTrigger>
-          <DropdownMenuContent className="dark">
-            <DropdownMenuItem>Price: Low to High</DropdownMenuItem>
-            <DropdownMenuItem>Price: High to Low</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-
-      <div className='m-4'>
-        <p>Products</p>
-
+        <p>Featured</p>
+        {loading &&
+          <div className="flex justify-center">
+            <LoadingSpinner className="w-32 h-32" />
+          </div>
+        }
+        {!loading &&
+          <Carousel className='w-full'>
+            <CarouselContent>
+              {
+                featuredProducts?.map((product) => {
+                  return (
+                    <CarouselItem className='basis-1/5' key={product.id}
+                      onClick={() =>
+                        router.push(`/productPage/${product.collectionName}/${product.id}`)
+                      }
+                      style={{ cursor: "pointer" }}>
+                      <ProductCard
+                        key={product.id}
+                        name={product.name}
+                        price={product.price}
+                        imageSrc={product.imageSrc}
+                      />
+                    </CarouselItem>
+                  );
+                })}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        }
         {/* {loading && <LoadingSpinner />} */}
         {error && <p>There was an error loading the products</p>}
-        {/* {!loading && !error && products &&
-          <div className='my-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 place-items-center'>
-            {products.map(product => (
-              <ProductCard key={product.id} name={product.name} price={product.price} imageSrc={product.imageSrc} />
-            ))}
-          </div>} */}
       </div>
 
       <CartSheet onOpenChange={setCartOpen} open={cartOpen} />
-      <div className="p-[2rem] grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 place-items-center">
-        {loading && products ? (
-          <LoadingSpinner />
-        ) : (
-          products?.map((Product) => {
-            return (
-              <div
-                key={Product.id}
-                onClick={() =>
-                  router.push(`/productPage/${Product.collectionName}/${Product.id}`)
-                }
-                style={{ cursor: "pointer" }}
-              >
-                <ProductCard
-                  name={Product.name}
-                  price={Product.price}
-                  imageSrc={Product.imageSrc}
-                />
-              </div>
-            );
-          })
-        )}
-      </div>
     </div>
-  );  
+  );
 }
